d1b5d72eda5f0536505593b3f657a5c196e7a6bd
김제혁 <kwpgur9764@gmail.com>
2019-10-12 03:58:00 +0900

    maple jump

---
+++ Assets/Scenes/Temp/01 kjh/Down.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Down : MonoBehaviour
+{
+    private PlatformEffector2D effetor;
+    public float waitTime;
+    public float ReversTime;
+   
+    void Start()
+    {
+        effetor = GetComponent<PlatformEffector2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (effetor.rotationalOffset == 180f)
+        {
+
+            ReversTime += ReversTime*Time.deltaTime;
+        }
+        if (Input.GetKeyUp(KeyCode.DownArrow))
+        {
+            waitTime = 0.4f;
+            ReversTime = 0.1f;
+
+        }
+       
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            if (waitTime <= 0.1f)
+            {
+                effetor.rotationalOffset = 180f;
+                waitTime = 0.4f;
+                
+            }
+            else { waitTime -= waitTime*Time.deltaTime; } 
+        }
+        if (Input.GetKey(KeyCode.Space)||ReversTime>=0.135f )
+        {
+            effetor.rotationalOffset = 0f;
+        }
+    }
+}
